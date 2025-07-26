d823c5f538350314ca4f2d59c8ac97981f9ae7bc
Oma <jayrockloryv@gmail.com>
2025-07-26 02:58:52 -0700

    New Project setup

---
+++ @latest/src/Pages/Catalogue.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar2 from "../Components/Navbar2";
+import Hero2 from "../Components/Hero2";
+import ProductGrid2 from "../Components/ProductGrid2";
+import Footer2 from "../Components/Footer";
+
+const Catalogue = () => {
+  return (
+    <div>
+      <Navbar2 />
+      <Hero2 />
+      <ProductGrid2 />
+      <Footer2 />
+    </div>
+  );
+};
+
+export default Catalogue;
---
+++ @latest/src/Pages/Hompage.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Navbar from "../Components/Navbar";
+import Hero from "../Components/Hero";
+import ProductGrid from "../Components/ProductGrid";
+import Footer2 from "../Components/Footer";
+
+const Hompage = () => {
+  return (
+    <div>
+      <Navbar />
+      <Hero />
+      <ProductGrid />
+      <Footer2 />
+    </div>
+  );
+};
+
+export default Hompage;
